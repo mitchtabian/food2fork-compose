bf5126a51776f5b1567a35f9f42a9d3764e4dbd0
mitch <mitch@tabian.ca>
2021-02-01 11:36:34 -0800

    cleanup

--- app/src/main/java/com/codingwithmitch/food2forkcompose/presentation/components/RecipeView.kt
+++ app/src/main/java/com/codingwithmitch/food2forkcompose/presentation/components/RecipeView.kt
@@ -1,8 +1,8 @@
 package com.codingwithmitch.food2forkcompose.presentation.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,70 +21,72 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 fun RecipeView(
     recipe: Recipe,
 ){
-    ScrollableColumn(
+    LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        val image = loadPicture(url = recipe.featuredImage, defaultImage = DEFAULT_RECIPE_IMAGE).value
-        image?.let { img ->
-            Image(
-                bitmap = img.asImageBitmap(),
-                contentDescription = "Recipe Featured Image",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .preferredHeight(IMAGE_HEIGHT.dp)
-                ,
-                contentScale = ContentScale.Crop,
-            )
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 4.dp)
-            ){
-                Text(
-                    text = recipe.title,
-                    modifier = Modifier
-                        .fillMaxWidth(0.85f)
-                        .wrapContentWidth(Alignment.Start)
-                    ,
-                    style = MaterialTheme.typography.h3
-                )
-                val rank = recipe.rating.toString()
-                Text(
-                    text = rank,
+        item {
+            val image = loadPicture(url = recipe.featuredImage, defaultImage = DEFAULT_RECIPE_IMAGE).value
+            image?.let { img ->
+                Image(
+                    bitmap = img.asImageBitmap(),
+                    contentDescription = "Recipe Featured Image",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .wrapContentWidth(Alignment.End)
-                        .align(Alignment.CenterVertically)
+                        .preferredHeight(IMAGE_HEIGHT.dp)
                     ,
-                    style = MaterialTheme.typography.h5
+                    contentScale = ContentScale.Crop,
                 )
             }
-            val updated = recipe.dateUpdated
-            Text(
-                text = "Updated ${updated} by ${recipe.publisher}"
-                ,
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 8.dp)
-                ,
-                style = MaterialTheme.typography.caption
-            )
-            for(ingredient in recipe.ingredients){
-                Text(
-                    text = ingredient,
+                    .padding(8.dp)
+            ) {
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(bottom = 4.dp)
+                ){
+                    Text(
+                        text = recipe.title,
+                        modifier = Modifier
+                            .fillMaxWidth(0.85f)
+                            .wrapContentWidth(Alignment.Start)
+                        ,
+                        style = MaterialTheme.typography.h3
+                    )
+                    val rank = recipe.rating.toString()
+                    Text(
+                        text = rank,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.End)
+                            .align(Alignment.CenterVertically)
+                        ,
+                        style = MaterialTheme.typography.h5
+                    )
+                }
+                val updated = recipe.dateUpdated
+                Text(
+                    text = "Updated ${updated} by ${recipe.publisher}"
                     ,
-                    style = MaterialTheme.typography.body1
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp)
+                    ,
+                    style = MaterialTheme.typography.caption
                 )
+                for(ingredient in recipe.ingredients){
+                    Text(
+                        text = ingredient,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 4.dp)
+                        ,
+                        style = MaterialTheme.typography.body1
+                    )
+                }
             }
         }
     }
