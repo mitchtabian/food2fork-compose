ea3642222b9e8ba54e1a7e7bb00eb9a4c4b2197e
mitch <mitch@tabian.ca>
2021-02-08 12:31:49 -0800

    use kotlin buildSrc for dependency management.

---
+++ buildSrc/src/main/java/dependencies/InstrumentationTest.kt
@@ -0,0 +1,6 @@
+package dependencies
+
+object InstrumentationTest {
+
+  const val compose_ui = "androidx.compose.ui:ui-test-junit4:${Versions.compose}"
+}
---
+++ buildSrc/src/main/java/dependencies/Jetbrains.kt
@@ -0,0 +1,6 @@
+package dependencies
+
+object Jetbrains {
+
+  const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+}
