5b19416fc6ad364f12b85d6a2b51d50cb9d53d9d
mitch <mitch@tabian.ca>
2021-02-12 08:50:21 -0800

    Add CacheModule.kt

---
+++ app/src/main/java/com/codingwithmitch/food2forkcompose/di/CacheModule.kt
@@ -0,0 +1,33 @@
+package com.codingwithmitch.food2forkcompose.di
+
+import androidx.room.Room
+import com.codingwithmitch.food2forkcompose.cache.RecipeDao
+import com.codingwithmitch.food2forkcompose.cache.database.AppDatabase
+import com.codingwithmitch.food2forkcompose.presentation.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CacheModule {
+
+  @Singleton
+  @Provides
+  fun provideDb(app: BaseApplication): AppDatabase {
+    return Room
+      .databaseBuilder(app, AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+      .fallbackToDestructiveMigration()
+      .build()
+  }
+
+  @Singleton
+  @Provides
+  fun provideRecipeDao(db: AppDatabase): RecipeDao{
+    return db.recipeDao()
+  }
+
+}
+
